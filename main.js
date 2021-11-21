29b483f0133a876a104ee45692bfe9d499ceebf6
season1618 <seasonminority314@gmail.com>
2021-11-21 12:48:58 +0900

    add eventlistener to editor, buttons

--- main.js
+++ main.js
@@ -3,108 +3,130 @@ import {Point, Line, Circle} from './modules/construction.js';
 import {dist, intrsecLines, intrsecLineAndCircle, intrsecCircles} from './modules/construction.js';
 import {codeData, parse} from './modules/parse.js';
 
-codeString = 'p1 = (-100, 0);\np2 = (100, 0);\nd1 = {p1, p2};\nl1 = (p1, p2);\nc1 = (p1, 150);\nc2 = (p2, 150);\n[p3, p4] = {c1, c2};\nl2 = (p3, p4);\np5 = {l1, l2};\nc3 = (p5, d1);\n[p6, p7] = {l2, c3};\nl3 = (p1, p7);\nd2 = {p1, p5};\nc4 = (p7, d2);\n[p8, p9] = {l3, c4};\nd3 = {p1, p9};\nc5 = (p1, d3);\n[p10, p11] = {l2, c5};\nc6 = (p1, d1);\nc7 = (p2, d1);\nc8 = (p11, d1);\n[p12, p13] = {c6, c8};\n[p14, p15] = {c7, c8};\nl4 = (p1, p13);\nl5 = (p11, p13);\nl6 = (p2, p14);\nl7 = (p11, p14);';
-parse(codeString);
+/*let codeString = 'p1 = (-100, 0);\np2 = (100, 0);\nd1 = {p1, p2};\nl1 = (p1, p2);\nc1 = (p1, 150);\nc2 = (p2, 150);\n[p3, p4] = {c1, c2};\nl2 = (p3, p4);\np5 = {l1, l2};\nc3 = (p5, d1);\n[p6, p7] = {l2, c3};\nl3 = (p1, p7);\nd2 = {p1, p5};\nc4 = (p7, d2);\n[p8, p9] = {l3, c4};\nd3 = {p1, p9};\nc5 = (p1, d3);\n[p10, p11] = {l2, c5};\nc6 = (p1, d1);\nc7 = (p2, d1);\nc8 = (p11, d1);\n[p12, p13] = {c6, c8};\n[p14, p15] = {c7, c8};\nl4 = (p1, p13);\nl5 = (p11, p13);\nl6 = (p2, p14);\nl7 = (p11, p14);';*/
 
-let varName = {};
-for(let i = 0; i < codeData.length; i++){
-    let lv1Name = codeData[i].lv1Name;
-    let lv2Name = codeData[i].lv2Name;
-    let op = codeData[i].op;
-    let rv1Name = codeData[i].rv1Name;
-    let rv2Name = codeData[i].rv2Name;
+let editor = document.getElementById('editor');
+let drawButton = document.getElementById('draw');
+let playButton = document.getElementById('play');
 
-    let rv1Type, rv2Type;
-    if(!isNaN(Number(rv1Name))){
-        varName[rv1Name] = Number(rv1Name);
-        rv1Type = 'number';
+drawButton.addEventListener(
+    'click',
+    function(){
+        let codeString = editor.value;
+        parse(codeString);
+        registerSketch();
+        draw(ctx);
     }
-    else if(rv1Name in varName){
-        if(typeof(varName[rv1Name]) == 'number') rv1Type = 'number';
-        else rv1Type = varName[rv1Name].type;
-    }
-    else{
-        console.log('sentence ' + i + 1 + ' : ' + rv1Name + ' is undefined.');
-        break;
-    }
-    if(!isNaN(Number(rv2Name))){
-        varName[rv2Name] = Number(rv2Name);
-        rv2Type = 'number';
-    }
-    else if(rv2Name in varName){
-        if(typeof(varName[rv2Name]) == 'number') rv2Type = 'number';
-        else rv2Type = varName[rv2Name].type;
-    }
-    else{
-        console.log('sentence ' + i + 1 + ' : ' + rv2Name + ' is undefined.');
-        break;
-    }
+);
 
-    if(op == '('){
-        if(rv1Type == 'number' && rv2Type == 'number'){
-            let x = canvas.width / 2 + Number(rv1Name);
-            let y = canvas.height / 2 - Number(rv2Name);
-            p = new Point(x, y);
-            sketches.push(p);
-            varName[lv1Name] = p;
+playButton.addEventListener(
+    'click',
+    function(){
+        let codeString = editor.value;
+        parse(codeString);
+        registerSketch();
+        animation(ctx);
+    }
+)
+
+function registerSketch(){
+    let varName = {};
+    for(let i = 0; i < codeData.length; i++){
+        let lv1Name = codeData[i].lv1Name;
+        let lv2Name = codeData[i].lv2Name;
+        let op = codeData[i].op;
+        let rv1Name = codeData[i].rv1Name;
+        let rv2Name = codeData[i].rv2Name;
+
+        let rv1Type, rv2Type;
+        if(!isNaN(Number(rv1Name))){
+            varName[rv1Name] = Number(rv1Name);
+            rv1Type = 'number';
         }
-        else if(rv1Type == 'Point' && rv2Type == 'Point'){
-            let p1 = varName[rv1Name];
-            let p2 = varName[rv2Name];
-            l = new Line(p1, p2);
-            sketches.push(l);
-            varName[lv1Name] = l;
-        }
-        else if(rv1Type == 'Point' && rv2Type == 'number'){
-            let p = varName[rv1Name];
-            let r = varName[rv2Name];
-            c = new Circle(p, r);
-            sketches.push(c);
-            varName[lv1Name] = c;
+        else if(rv1Name in varName){
+            if(typeof(varName[rv1Name]) == 'number') rv1Type = 'number';
+            else rv1Type = varName[rv1Name].type;
         }
         else{
-            console.log('sentence ' + i + 1 + ': { ' + rv1Type + ', ' + rv2Type + ' } is invalid.');
+            console.log('sentence ' + i + 1 + ' : ' + rv1Name + ' is undefined.');
             break;
         }
-    }
-    else if(op == '{'){
-        if(rv1Type == 'Point' && rv2Type == 'Point'){
-            let p1 = varName[rv1Name];
-            let p2 = varName[rv2Name];
-            let d = dist(p1, p2);
-            varName[lv1Name] = d;
+        if(!isNaN(Number(rv2Name))){
+            varName[rv2Name] = Number(rv2Name);
+            rv2Type = 'number';
         }
-        else if(rv1Type == 'Line' && rv2Type == 'Line'){
-            let l1 = varName[rv1Name];
-            let l2 = varName[rv2Name];
-            let p = intrsecLines(l1, l2);
-            sketches.push(p);
-            varName[lv1Name] = p;
-        }
-        else if(rv1Type == 'Line' && rv2Type == 'Circle'){
-            let l = varName[rv1Name];
-            let c = varName[rv2Name];
-            let [p1, p2] = intrsecLineAndCircle(l, c);
-            sketches.push(p1);
-            sketches.push(p2);
-            varName[lv1Name] = p1;
-            varName[lv2Name] = p2;
-        }
-        else if(rv1Type == 'Circle' && rv2Type == 'Circle'){
-            let c1 = varName[rv1Name];
-            let c2 = varName[rv2Name];
-            let [p1, p2] = intrsecCircles(c1, c2);
-            sketches.push(p1);
-            sketches.push(p2);
-            varName[lv1Name] = p1;
-            varName[lv2Name] = p2;
+        else if(rv2Name in varName){
+            if(typeof(varName[rv2Name]) == 'number') rv2Type = 'number';
+            else rv2Type = varName[rv2Name].type;
         }
         else{
-            console.log('sentence ' + i + 1 + ': { ' + rv1Type + ', ' + rv2Type + ' } is invalid.');
+            console.log('sentence ' + i + 1 + ' : ' + rv2Name + ' is undefined.');
             break;
+        }
+
+        if(op == '('){
+            if(rv1Type == 'number' && rv2Type == 'number'){
+                let x = canvas.width / 2 + Number(rv1Name);
+                let y = canvas.height / 2 - Number(rv2Name);
+                p = new Point(x, y);
+                sketches.push(p);
+                varName[lv1Name] = p;
+            }
+            else if(rv1Type == 'Point' && rv2Type == 'Point'){
+                let p1 = varName[rv1Name];
+                let p2 = varName[rv2Name];
+                l = new Line(p1, p2);
+                sketches.push(l);
+                varName[lv1Name] = l;
+            }
+            else if(rv1Type == 'Point' && rv2Type == 'number'){
+                let p = varName[rv1Name];
+                let r = varName[rv2Name];
+                c = new Circle(p, r);
+                sketches.push(c);
+                varName[lv1Name] = c;
+            }
+            else{
+                console.log('sentence ' + i + 1 + ': { ' + rv1Type + ', ' + rv2Type + ' } is invalid.');
+                break;
+            }
+        }
+        else if(op == '{'){
+            if(rv1Type == 'Point' && rv2Type == 'Point'){
+                let p1 = varName[rv1Name];
+                let p2 = varName[rv2Name];
+                let d = dist(p1, p2);
+                varName[lv1Name] = d;
+            }
+            else if(rv1Type == 'Line' && rv2Type == 'Line'){
+                let l1 = varName[rv1Name];
+                let l2 = varName[rv2Name];
+                let p = intrsecLines(l1, l2);
+                sketches.push(p);
+                varName[lv1Name] = p;
+            }
+            else if(rv1Type == 'Line' && rv2Type == 'Circle'){
+                let l = varName[rv1Name];
+                let c = varName[rv2Name];
+                let [p1, p2] = intrsecLineAndCircle(l, c);
+                sketches.push(p1);
+                sketches.push(p2);
+                varName[lv1Name] = p1;
+                varName[lv2Name] = p2;
+            }
+            else if(rv1Type == 'Circle' && rv2Type == 'Circle'){
+                let c1 = varName[rv1Name];
+                let c2 = varName[rv2Name];
+                let [p1, p2] = intrsecCircles(c1, c2);
+                sketches.push(p1);
+                sketches.push(p2);
+                varName[lv1Name] = p1;
+                varName[lv2Name] = p2;
+            }
+            else{
+                console.log('sentence ' + i + 1 + ': { ' + rv1Type + ', ' + rv2Type + ' } is invalid.');
+                break;
+            }
         }
     }
 }
-
-draw(ctx);
-//animation();
